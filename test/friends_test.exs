56e476c60fdce50f617de50c2e113e2fb8a6be13
zwpdbh <hyperion_z@outlook.com>
2023-04-27 18:02:57 +0800

    <update

--- test/friends_test.exs
+++ test/friends_test.exs
@@ -13,12 +13,23 @@ defmodule FriendsTest do
     assert Friends.hello() == :world
   end
 
-  test "test database" do
+  test "test database 01" do
     result =
       Friends.Person.demo_query_all()
       |> IO.inspect()
       |> length()
 
     assert result == 3
+  end
+
+  test "test database 02" do
+    Friends.Person.demo_delete()
+
+    result =
+      Friends.Person.demo_query_all()
+      |> IO.inspect()
+      |> length()
+
+    assert result == 2
   end
 end
